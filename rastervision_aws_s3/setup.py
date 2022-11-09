84a9995af987c6a3e81767ad4748bac8c0a72d84
Nkosenhle Duma <personal.godsonduma@gmail.com>
2022-11-09 13:05:03 +0200

    Updating setup.py to enable pip installing git subdependencies

--- rastervision_aws_s3/setup.py
+++ rastervision_aws_s3/setup.py
@@ -2,12 +2,37 @@
 
 from os import path as op
 import io
+from urllib.parse import urlparse
+
 from setuptools import (setup, find_namespace_packages)
 
 here = op.abspath(op.dirname(__file__))
 with io.open(op.join(here, 'requirements.txt'), encoding='utf-8') as f:
     all_reqs = f.read().split('\n')
+
+
+def create_git_install_requires(all_requirements: list) -> list:
+    git_install_requires = []
+    # if git+, get package name and add below into requires list
+    # install_requires = [
+    #   'some-pkg @ git+ssh://git@github.com/someorgname/pkg-repo-name@v1.1#egg=some-pkg',
+    # ]
+    for req in all_requirements:
+        if 'git+' not in req:
+            continue
+        # req is git url
+        # git+https://github.com/215023375/raster-vision.git@mainline#egg=rastervision_pipeline&subdirectory=rastervision_pipeline
+        parsed_url = urlparse(req)
+        url_fragments = parsed_url.fragment.split('&')
+        package_name = [fragment for fragment in url_fragments if 'egg=' in fragment][0].replace("egg=", "")
+        
+        git_install_requires.append(f"{package_name} @ {req}")
+
+    return git_install_requires
+
+
 install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
+all_install_requires = [*install_requires, *create_git_install_requires(all_reqs)]
 
 name = 'rastervision_aws_s3'
 version = '0.14.0'
@@ -30,5 +55,5 @@ setup(
     keywords=
     'raster deep-learning ml computer-vision earth-observation geospatial geospatial-processing',
     packages=find_namespace_packages(exclude=['integration_tests*', 'tests*']),
-    install_requires=install_requires,
+    install_requires=all_install_requires,
     zip_safe=False)
--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ import io
 import re
 from setuptools import (setup, find_namespace_packages)
 from imp import load_source
+from urllib.parse import urlparse
 
 here = op.abspath(op.dirname(__file__))
 __version__ = '0.14.0'
@@ -21,7 +22,35 @@ if 'READTHEDOCS' in os.environ:
     all_reqs = list(
         filter(lambda r: r.split('==')[0] not in bad_reqs, all_reqs))
 
+
+def create_git_install_requires(all_requirements: list) -> list:
+    """
+    https://stackoverflow.com/questions/32688688/how-to-write-setup-py-to-include-a-git-repository-as-a-dependency
+    :param all_requirements:
+    :return:
+    """
+
+    git_install_requires = []
+    # if git+, get package name and add below into requires list
+    # install_requires = [
+    #   'some-pkg @ git+ssh://git@github.com/someorgname/pkg-repo-name@v1.1#egg=some-pkg',
+    # ]
+    for req in all_requirements:
+        if 'git+' not in req:
+            continue
+        # req is git url
+        # git+https://github.com/215023375/raster-vision.git@mainline#egg=rastervision_pipeline&subdirectory=rastervision_pipeline
+        parsed_url = urlparse(req)
+        url_fragments = parsed_url.fragment.split('&')
+        package_name = [fragment for fragment in url_fragments if 'egg=' in fragment][0].replace("egg=", "")
+        
+        git_install_requires.append(f"{package_name} @ {req}")
+
+    return git_install_requires
+
+
 install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
+all_install_requires = [*install_requires, *create_git_install_requires(all_reqs)]
 
 
 def replace_images(readme):
@@ -58,4 +87,4 @@ setup(
     'raster deep-learning ml computer-vision earth-observation geospatial geospatial-processing',
     packages=[],
     include_package_data=True,
-    install_requires=install_requires)
+    install_requires=all_install_requires)
