7b69cfb1eacbb590d5192f695e2e3191ac73cb71
Nkosenhle Duma <personal.godsonduma@gmail.com>
2022-12-18 18:54:28 +0200

    Using symlink check for lib64

--- .conda/etc/conda/activate.d/update_ld_library_path.sh
+++ .conda/etc/conda/activate.d/update_ld_library_path.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-# Create $CONDA_PREFIX/lib64 dir
-ln -s $CONDA_PREFIX/lib $CONDA_PREFIX/lib64
+# Create $CONDA_PREFIX/lib64 dir (check if link exists)
+[ ! -L "$CONDA_PREFIX"/lib64 ] && ln -s "$CONDA_PREFIX"/lib "$CONDA_PREFIX"/lib64
 
 export _OLD_LD_LIBRARY_PATH=$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:$LD_LIBRARY_PATH"
--- .conda/etc/conda/deactivate.d/unset_ld_library_path.sh
+++ .conda/etc/conda/deactivate.d/unset_ld_library_path.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-rm $CONDA_PREFIX/lib64
+[ -L "$CONDA_PREFIX"/lib64 ] && rm "$CONDA_PREFIX"/lib64
 
 export LD_LIBRARY_PATH=$_OLD_LD_LIBRARY_PATH
 unset _OLD_LD_LIBRARY_PATH
