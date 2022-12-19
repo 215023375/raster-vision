7e4abd5a07730cdf7d2b087c0e488c634570c169
Nkosenhle Duma <personal.godsonduma@gmail.com>
2022-12-20 00:12:40 +0200

    Updating rastervision_customs and adding rastervision_customs directories to the PYTHONPATH

--- .conda/etc/conda/activate.d/update_ld_library_path.sh
+++ .conda/etc/conda/activate.d/update_ld_library_path.sh
@@ -6,3 +6,14 @@
 export _OLD_LD_LIBRARY_PATH=$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:$LD_LIBRARY_PATH"
 
+# TODO: Create a separate script  for the PYTHONPATH (or rename this - and update symlink)
+PYTHONPATH="$PROJET_DIR/rastervision_aws_batch:$PYTHONPATH"
+PYTHONPATH="$PROJET_DIR/rastervision_aws_s3:$PYTHONPATH"
+PYTHONPATH="$PROJET_DIR/rastervision_core:$PYTHONPATH"
+PYTHONPATH="$PROJET_DIR/rastervision_customs:$PYTHONPATH"
+PYTHONPATH="$PROJET_DIR/rastervision_gdal_vsi:$PYTHONPATH"
+PYTHONPATH="$PROJET_DIR/rastervision_pipeline:$PYTHONPATH"
+PYTHONPATH="$PROJET_DIR/rastervision_pytorch_backend:$PYTHONPATH"
+PYTHONPATH="$PROJET_DIR/rastervision_pytorch_learner:$PYTHONPATH"
+
+export PYTHONPATH
