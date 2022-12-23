5b69e989779ac66200306f27788fb4ba4e5b16d6
Nkosenhle Duma <personal.godsonduma@gmail.com>
2022-12-23 05:14:35 +0200

    Updating conda scripts

--- .conda/create_etc_conda_scripts.sh
+++ .conda/create_etc_conda_scripts.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
-ln -s $(pwd)/.conda/etc/conda/activate.d/update_ld_library_path.sh $CONDA_PREFIX/etc/conda/activate.d/
-ln -s $(pwd)/.conda/etc/conda/deactivate.d/unset_ld_library_path.sh $CONDA_PREFIX/etc/conda/deactivate.d/
+# Assuming conda environment name is 'rastervision-poc' as in the conda.env.yaml
+CONDA_ENVIRONMENT_NAME=~/anaconda3/envs/rastervision-poc
+ln -s $(pwd)/.conda/etc/conda/activate.d/update_ld_library_path.sh $CONDA_ENVIRONMENT_NAME/etc/conda/activate.d/
+ln -s $(pwd)/.conda/etc/conda/deactivate.d/unset_ld_library_path.sh $CONDA_ENVIRONMENT_NAME/etc/conda/deactivate.d/
--- .conda/etc/conda/activate.d/update_ld_library_path.sh
+++ .conda/etc/conda/activate.d/update_ld_library_path.sh
@@ -6,14 +6,16 @@
 export _OLD_LD_LIBRARY_PATH=$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:$LD_LIBRARY_PATH"
 
+
 # TODO: Create a separate script  for the PYTHONPATH (or rename this - and update symlink)
-PYTHONPATH="$PROJET_DIR/rastervision_aws_batch:$PYTHONPATH"
-PYTHONPATH="$PROJET_DIR/rastervision_aws_s3:$PYTHONPATH"
-PYTHONPATH="$PROJET_DIR/rastervision_core:$PYTHONPATH"
-PYTHONPATH="$PROJET_DIR/rastervision_customs:$PYTHONPATH"
-PYTHONPATH="$PROJET_DIR/rastervision_gdal_vsi:$PYTHONPATH"
-PYTHONPATH="$PROJET_DIR/rastervision_pipeline:$PYTHONPATH"
-PYTHONPATH="$PROJET_DIR/rastervision_pytorch_backend:$PYTHONPATH"
-PYTHONPATH="$PROJET_DIR/rastervision_pytorch_learner:$PYTHONPATH"
+PROJECT_DIR="$HOME/workspace/lucc/submodules/raster-vision"
+PYTHONPATH="$PROJECT_DIR/rastervision_aws_batch:$PYTHONPATH"
+PYTHONPATH="$PROJECT_DIR/rastervision_aws_s3:$PYTHONPATH"
+PYTHONPATH="$PROJECT_DIR/rastervision_core:$PYTHONPATH"
+PYTHONPATH="$PROJECT_DIR/rastervision_customs:$PYTHONPATH"
+PYTHONPATH="$PROJECT_DIR/rastervision_gdal_vsi:$PYTHONPATH"
+PYTHONPATH="$PROJECT_DIR/rastervision_pipeline:$PYTHONPATH"
+PYTHONPATH="$PROJECT_DIR/rastervision_pytorch_backend:$PYTHONPATH"
+PYTHONPATH="$PROJECT_DIR/rastervision_pytorch_learner:$PYTHONPATH"
 
 export PYTHONPATH
